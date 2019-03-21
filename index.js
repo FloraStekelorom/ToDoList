98f149d29c2ac8522f6c28eec07f89008dad6688
Flora Hewitt <flora.stekelorom@gmail.com>
2019-03-21 11:10:59 +0400

    wrote the 'add a new task' functionality'

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
+var printTodoList = function() {
   $.ajax({
     type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=74',
     dataType: 'json',
     success: function (response, textStatus) {
+      $('#todoList').empty();
       response.tasks.forEach(function (task) {
         $('#todoList').append('<p>' + task.content + '</p>');
       })
@@ -12,4 +13,33 @@ $(document).ready(function(){
       console.log(errorMessage);
     }
   });
+}
+
+var createTask = function () {
+  $.ajax({
+    type: 'POST',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=74',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: $('#inputBox').val()
+      }
+    }),
+    success: function (response, textStatus) {
+      $('#inputBox').val('');
+      printTodoList();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+$(document).ready(function(){
+  printTodoList();
+   $('#addTask').on('submit', function(event) {
+     event.preventDefault();
+     createTask();
+   });
 });
